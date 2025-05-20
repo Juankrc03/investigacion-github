3a08ef9d041655756ac37b8a2fc44025c7413424
Juankrc03 <juankriosc1@gmail.com>
2025-05-20 16:28:31 -0600

    se corrigio todo el codigo y ya funciona suma y resta

--- investigacion.cpp
+++ investigacion.cpp
@@ -2,33 +2,27 @@
 using namespace std;
 
 // Prototipos de funciones
-
-int sumar(int a, int b) {
-    return a + b;
-}
-int restar(int a, int b) {
-    return a - b;
-}
-
+int sumar(int a, int b);
+int restar(int a, int b);
 int multiplicar(int a, int b);
 float dividir(int a, int b);
 
 int main() {
     int opcion, num1, num2;
-
-    cout << "==== Calculadora simple ====" << endl;
+    
+    cout << "===== Calculadora Simple =====" << endl;
     cout << "1. Sumar" << endl;
     cout << "2. Restar" << endl;
     cout << "3. Multiplicar" << endl;
     cout << "4. Dividir" << endl;
-    cout << "Elija una opción: ";
+    cout << "Elija una opcion: ";
     cin >> opcion;
-
+    
     cout << "Ingrese el primer número: ";
     cin >> num1;
     cout << "Ingrese el segundo número: ";
     cin >> num2;
-
+    
     switch(opcion) {
         case 1:
             cout << "Resultado: " << sumar(num1, num2) << endl;
@@ -49,6 +43,23 @@ int main() {
         default:
             cout << "Opción inválida." << endl;
     }
-
+    
     return 0;
+}
+
+// Implementaciones de las funciones
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int restar(int a, int b) {
+    return a - b;
+}
+
+int multiplicar(int a, int b) {
+    return a * b;
+}
+
+float dividir(int a, int b) {
+    return (float)a / b;
 }
